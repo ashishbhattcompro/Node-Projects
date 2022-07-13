6de6a247d481898054b3f4936691d3ab1dadfded
Ashish <ashish.bhatt@comprotechnologies.com>
2022-07-13 17:46:44 +0530

    first-commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 require('./router')(app);
-const config = require('./config.json');
+const prod = process.env.NODE_ENV === "production" ? "prod" : "dev";
+const config = require(`./config/${prod}.json`);
 const port = config.server.port;
-const host = config.server.host ;
 
 app.listen(port, () => {
     console.log(`app1 listening on port ${port}`)
